6397999641f4ecc9b9f536e61418e194b136bd29
AmolSonawane1026 <sonawaneamol787@gmail.com>
2025-04-20 23:00:49 +0530

    make the code responsive for devices now working on images lazy loading and optimization

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 import Logo from "../../assets/Logo/Logo.png"
+import { Menu, X } from 'lucide-react'
 
 const Navbar = ({ onOpen }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen)
+    }
   return (
    <>
     <nav className='navbar'>
@@ -11,18 +17,27 @@ const Navbar = ({ onOpen }) => {
             <img src={Logo} alt="Logo" />
         </div>
 
-        <div className="middle">
+        <div className={`middle ${isMenuOpen ? 'show' : ''}`}>
             <ul className="ul_list">
                 <li className="li_list">About Us</li>
                 <li className="li_list">Features</li>
                 <li className="li_list">More Option</li>
                 <li className="li_list">Contact</li>
             </ul>
-        </div>
 
-        <div className="buttons">
+            <div className="buttons mobile_buttons">
             <button className="btn log_in">Log In</button>
             <button className="btn sign_up" onClick={onOpen}>Sign Up</button>
+          </div>
+        </div>
+
+        <div className="buttons desktop_buttons">
+          <button className="btn log_in">Log In</button>
+          <button className="btn sign_up" onClick={onOpen}>Sign Up</button>
+        </div>
+
+        <div className="hamburger" onClick={toggleMenu}>
+          {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
         </div>
     </nav>
    </>
